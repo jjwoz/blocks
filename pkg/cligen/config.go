c3a874550b3ea00d76d737a2721e91f7d324d5d8
John Wozniak <johnwozniak@Johns-MacBook-Pro.local>
2022-01-11 14:14:32 -0500

    currently reading in flags to fetch information

--- pkg/cligen/config.go
+++ pkg/cligen/config.go
@@ -7,10 +7,16 @@ package cligen
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	polyConfig string
+	apiKey     string
 )
 
 // configCmd represents the config command
@@ -19,14 +25,15 @@ var configCmd = &cobra.Command{
 	Short: "upload latest config",
 	Long: `upload the poc latest config so the cli can read in the config and proceed with 
 			reading in the roundStartTime and roundEndTime.`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		//TODO:
 		// 1. start with reading in string args and printing it back out
 		// 2. allow for user to paste in path for the config and parse the json and read the file
 		// 3. once read fetch the data from polyscan
-		fmt.Println("config called start reading.....")
-		fmt.Println("\n")
-		readArgs(args)
+		if err := readArgs(args); err != nil {
+			return err
+		}
+		return nil
 	},
 }
 
@@ -48,41 +55,53 @@ type PolyscanResponse struct {
 	Result  string `json:"result"`
 }
 
-func readArgs(args []string) {
+func readArgs(args []string) error {
 	var cla CommandLineArgs
 
 	cla.RoundStartTime = args[0]
 	cla.RoundEndTime = args[1]
 
-	res := fetchBlockData(cla)
+	res, err := fetchBlockData(cla)
+	if err != nil {
+		return err
+	}
+
 	r, err := json.MarshalIndent(res, "", "  ")
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
 
 	fmt.Println(string(r))
-
+	return nil
 }
 
-func fetchBlockData(cla CommandLineArgs) CommandLineResponse {
+func fetchBlockData(cla CommandLineArgs) (CommandLineResponse, error) {
 	var clr CommandLineResponse
+	var err error
 
-	apiKey := "ADD"
+	// TODO: move to viper
 	url := fmt.Sprintf("https://api.polygonscan.com/api?module=block&action=getblocknobytime&timestamp=%v&closest=before&apikey=%v", cla.RoundStartTime, apiKey)
 	url2 := fmt.Sprintf("https://api.polygonscan.com/api?module=block&action=getblocknobytime&timestamp=%v&closest=before&apikey=%v", cla.RoundEndTime, apiKey)
 	// fetch data
-	sb := get(url)
-	eb := get(url2)
+	sb, err := get(url)
+	if err != nil {
+		return clr, err
+	}
+	eb, err := get(url2)
 
 	clr.StartBlock = sb
 	clr.EndBlock = eb
 
-	return clr
+	return clr, nil
 }
 
-func get(url string) string {
+func get(url string) (string, error) {
 	// TODO: allow for making concurrent api calls - allowing to make both calls simultaneously
 	var res PolyscanResponse
+	if apiKey == "" {
+		err := fmt.Errorf("error reading in apikey")
+		return "", err
+	}
 
 	resp, err := http.Get(url)
 
@@ -101,7 +120,7 @@ func get(url string) string {
 		fmt.Println(err)
 	}
 
-	return res.Result
+	return res.Result, nil
 
 }
 
@@ -111,6 +130,13 @@ func init() {
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
+	addCmd.Flags().StringVar(&polyConfig, "poly-config", "", "pass the poc-polygon-latest.json")
+	addCmd.PersistentFlags().StringVar(&apiKey, "apikey", "", "provide the api key for polyscan")
+	err := viper.BindPFlag("apikey", addCmd.PersistentFlags().Lookup("apikey"))
+	if err != nil {
+		fmt.Println("ERROR WITH BIND")
+		return
+	}
 	// and all subcommands, e.g.:
 	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
 
